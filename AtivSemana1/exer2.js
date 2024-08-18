5bcf30cc14b14ef001f96a88592cd6186481fc80
Giovanni Fortunato <giovannifortunato1910@gmail.com>
2024-08-18 20:50:39 -0300

    gio

---
+++ AtivSemana1/exer2.js
@@ -0,0 +1,6 @@
+let x = prompt("Digite um número inteiro positivo:","");
+i=1;
+x = Number;
+for(i>0 && i<x){
+    
+}
--- javascript/exer2.js
+++
@@ -1,6 +0,0 @@
-let x = prompt("Digite um número inteiro positivo:","");
-i=1;
-x = Number;
-for(i>0 && i<x){
-    
-}
